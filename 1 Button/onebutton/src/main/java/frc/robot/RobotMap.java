c4633bac312716ba6fbd612c9e73269d16466e2f
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2019-12-05 21:45:49 -0600

    Initial Commit

---
+++ 1 Button/onebutton/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ * 
+ */
+public class RobotMap 
+{
+  public static enum CANSparkMaxID {
+    kMotor1(1, "Motor1");
+
+    public final int id;
+    public final String name;
+
+    private CANSparkMaxID(int id, String name) {
+      this.id = id;
+      this.name = name;
+    }
+  }
+
+public static final double driveValue = 1;;
+
+
+
+
+  
+
+
+
+
+
+}
+
---
+++ 1 Button/onebutton/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap.CANSparkMaxID;
+
+/**
+ * Add your docs here.
+ * 
+ * @param 
+ */
+public class DriveTrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  public static CANSparkMax m1SparkMax = new CANSparkMax(CANSparkMaxID.kMotor1.id, MotorType.kBrushless);
+
+
+
+  @Override
+  public void initDefaultCommand() {
+    
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+
+    
+
+
+  }
+
+}
